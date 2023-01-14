ac3319dffc18151c1caeb0ae7a898694f452a57f
m@nji <spontaneousmanji@gmail.com>
2023-01-14 01:19:42 +0100

    adding env file

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const http = require("http");
 const express = require("express");
-// require("dotenv").config();
+require("dotenv").config();
 const cors = require("cors");
-const PORT = 4000;
+const PORT = 4000 || process.env.PORT;
+console.log(PORT);
 const app = express();
 app.use(
   cors({
@@ -17,5 +18,5 @@ app.get("/", (req, res) => {
 
 const server = http.createServer(app);
 server.listen(PORT, () => {
-  console.log("server is ready!");
+  console.log(`server is ready! PORT::::${PORT}`);
 });
